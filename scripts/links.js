2b6f48c153c28b6be3d57f6c38c4580e32df944b
Francirlei <francirlei-silva2011@hotmail.com>
2026-01-01 18:00:56 -0300

    Adicionando informações sobre o Autor

--- scripts/links.js
+++ scripts/links.js
@@ -1,19 +1,33 @@
 const links = document.getElementById("links");
 const modalLink = document.getElementById("modal-links");
-const modalIconClose = document.querySelector(".modal__close__icon");
-const modalBtnClose = document.querySelector(".modal__close__btn");
+const modalIconClose = document.querySelectorAll(".modal__close__icon");
+const modalBtnClose = document.querySelectorAll(".modal__close__btn");
+
+const modalTarget = document.querySelectorAll("[data-modal-target]") 
 
 
-links.addEventListener("click", (el) => {
-  el.preventDefault();
+modalTarget.forEach((target) => {
+  const modal = document.querySelector(`[data-modal=${target.dataset.modalTarget}]`)
+  target.addEventListener("click", (el) => {
+    el.preventDefault();
 
-  modalLink.showModal();
+    modal.showModal();
+  });
+})
+
+
+modalIconClose.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    document.querySelectorAll("[data-modal]").forEach((modal) => {
+      modal.close();
+    });
+  });
 });
 
-modalIconClose.addEventListener("click", (el) => {
-  modalLink.close();
-});
-
-modalBtnClose.addEventListener("click", (el) => {
-  modalLink.close();
-});
+modalBtnClose.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    document.querySelectorAll("[data-modal]").forEach((modal) => {
+      modal.close();
+    });
+  });
+})
